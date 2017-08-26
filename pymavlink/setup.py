792647afd0ebfb8597704f54dc41219589f74c49
Vladimir Ermakov <vooon341@gmail.com>
2017-08-26 10:22:39 +0000

    Rebase from 'debian/kinetic/mavlink'

--- pymavlink/setup.py
+++ pymavlink/setup.py
@@ -1,5 +1,4 @@
-from __future__ import print_function
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 from setuptools.command.build_py import build_py
 # Work around mbcs bug in distutils.
 # http://bugs.python.org/issue10945
@@ -14,7 +13,7 @@ except LookupError:
 from setuptools import setup, Extension
 import glob, os, shutil, fnmatch, platform, sys
 
-version = '2.1.0'
+version = '2.2.4'
 
 
 def generate_content():
@@ -100,8 +99,12 @@ class custom_build_py(build_py):
 setup (name = 'pymavlink',
        version = version,
        description = 'Python MAVLink code',
-       long_description = '''A Python library for handling MAVLink protocol streams and log files. This allows for the creation of simple scripts to analyse telemetry logs from autopilots such as ArduPilot which use the MAVLink protocol. See the scripts that come with the package for examples of small, useful scripts that use pymavlink. For more information about the MAVLink protocol see http://qgroundcontrol.org/mavlink/''',
-       url = 'http://github.com/mavlink/mavlink',
+       long_description = ('A Python library for handling MAVLink protocol streams and log files. This allows for the '
+                           'creation of simple scripts to analyse telemetry logs from autopilots such as ArduPilot which use '
+                           'the MAVLink protocol. See the scripts that come with the package for examples of small, useful '
+                           'scripts that use pymavlink. For more information about the MAVLink protocol see '
+                           'http://qgroundcontrol.org/mavlink/'),
+       url = 'https://github.com/ArduPilot/pymavlink/',
        classifiers=['Development Status :: 4 - Beta',
                     'Environment :: Console',
                     'Intended Audience :: Science/Research',
