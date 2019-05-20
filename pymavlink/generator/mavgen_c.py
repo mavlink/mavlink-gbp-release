cff0644c38ab820693f3ce6938b8b1ae5eca09f3
Vladimir Ermakov <vooon341@gmail.com>
2019-05-20 18:25:49 +0000

    Rebase from 'debian/melodic/mavlink'

--- pymavlink/generator/mavgen_c.py
+++ pymavlink/generator/mavgen_c.py
@@ -586,9 +586,10 @@ def generate_one(basename, xml):
     if xml.command_24bit:
         # we sort with primary key msgid
         for msgid in sorted(xml.message_crcs.keys()):
-            xml.message_crcs_array += '{%u, %u, %u, %u, %u, %u}, ' % (msgid,
+            xml.message_crcs_array += '{%u, %u, %u, %u, %u, %u, %u}, ' % (msgid,
                                                                       xml.message_crcs[msgid],
                                                                       xml.message_min_lengths[msgid],
+                                                                      xml.message_lengths[msgid],
                                                                       xml.message_flags[msgid],
                                                                       xml.message_target_system_ofs[msgid],
                                                                       xml.message_target_component_ofs[msgid])
