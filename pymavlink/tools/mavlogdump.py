7d2c93ca2b82d154100ef92523b12b62d0fac222
Vladimir Ermakov <vooon341@gmail.com>
2019-10-10 12:03:36 +0000

    Rebase from 'release/kinetic/mavlink'

--- pymavlink/tools/mavlogdump.py
+++ pymavlink/tools/mavlogdump.py
@@ -13,6 +13,7 @@ import fnmatch
 import json
 import os
 import struct
+import sys
 import time
 
 try:
@@ -44,8 +45,12 @@ parser.add_argument("--show-types", action='store_true', help="Shows all message
 parser.add_argument("--source-system", type=int, default=None, help="filter by source system ID")
 parser.add_argument("--source-component", type=int, default=None, help="filter by source component ID")
 parser.add_argument("--link", type=int, default=None, help="filter by comms link ID")
+parser.add_argument("--verbose", action='store_true', help="Dump messages in a much more verbose (but non-parseable) format")
 parser.add_argument("--mav10", action='store_true', help="parse as MAVLink1")
+parser.add_argument("--reduce", type=int, default=0, help="reduce streaming messages")
 parser.add_argument("log", metavar="LOG")
+parser.add_argument("--profile", action='store_true', help="run the Yappi python profiler")
+
 args = parser.parse_args()
 
 if not args.mav10:
@@ -56,6 +61,9 @@ import inspect
 from pymavlink import mavutil
 
 
+if args.profile:
+    import yappi    # We do the import here so that we won't barf if run normally and yappi not available
+    yappi.start()
 
 filename = args.log
 mlog = mavutil.mavlink_connection(filename, planner_format=args.planner,
@@ -80,6 +88,32 @@ ext = os.path.splitext(filename)[1]
 isbin = ext in ['.bin', '.BIN', '.px4log']
 islog = ext in ['.log', '.LOG'] # NOTE: "islog" does not mean a tlog
 istlog = ext in ['.tlog', '.TLOG']
+
+# list of msgs to reduce in rate when --reduce is used
+reduction_msgs = ['NKF*', 'XKF*', 'IMU*', 'AHR2', 'BAR*', 'ATT', 'BAT*', 'CTUN', 'NTUN', 'GP*', 'IMT*', 'MAG*', 'PL', 'POS', 'POW*', 'RATE', 'RC*', 'RFND', 'UBX*', 'VIBE', 'NKQ*', 'MOT*', 'CTRL', 'FTS*', 'DSF', 'CST*', 'LOS*', 'UWB*']
+reduction_yes = set()
+reduction_no = set()
+reduction_count = {}
+
+def reduce_msg(mtype, reduction_ratio):
+    '''return True if this msg should be discarded by reduction'''
+    global reduction_count, reduction_msgs, reduction_yes, reduction_no
+    if mtype in reduction_no:
+        return False
+    if not mtype in reduction_yes:
+        for m in reduction_msgs:
+            if fnmatch.fnmatch(mtype, m):
+                reduction_yes.add(mtype)
+                reduction_count[mtype] = 0
+                break
+        if not mtype in reduction_yes:
+            reduction_no.add(mtype)
+            return False
+    reduction_count[mtype] += 1
+    if reduction_count[mtype] == reduction_ratio:
+        reduction_count[mtype] = 0
+        return False
+    return True
 
 if args.csv_sep == "tab":
     args.csv_sep = "\t"
@@ -151,6 +185,9 @@ while True:
             fields += m.Columns.split(',')
             csv_out = ["" for x in fields]
             print(args.csv_sep.join(fields))
+
+    if args.reduce and reduce_msg(m.get_type(), args.reduce):
+        continue
 
     if output is not None:
         if (isbin or islog) and m.get_type() == "FMT":
@@ -252,8 +289,14 @@ while True:
                 csv_out = [str(data[y]) if y != "timestamp" else "" for y in fields]
             else:
                 csv_out = [str(data[y.split('.')[-1]]) if y.split('.')[0] == type and y.split('.')[-1] in data else "" for y in fields]
-    # Otherwise we output in a standard Python dict-style format
+    elif args.show_types:
+        # do nothing
+        pass
+    elif args.verbose and istlog:
+        mavutil.dump_message_verbose(sys.stdout, m)
+        print("")
     else:
+        # Otherwise we output in a standard Python dict-style format
         s = "%s.%02u: %s" % (time.strftime("%Y-%m-%d %H:%M:%S",
                                            time.localtime(timestamp)),
                              int(timestamp*100.0)%100, m)
@@ -261,8 +304,7 @@ while True:
             s += " srcSystem=%u srcComponent=%u" % (m.get_srcSystem(), m.get_srcComponent())
         if args.show_seq:
             s += " seq=%u" % m.get_seq()
-        if not args.show_types:
-            print(s)
+        print(s)
 
     # Update our last timestamp value.
     last_timestamp = timestamp
@@ -270,3 +312,7 @@ while True:
 if args.show_types:
     for msgType in available_types:
         print(msgType)
+
+if args.profile:
+    yappi.get_func_stats().print_all()
+    yappi.get_thread_stats().print_all()
