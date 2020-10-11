70ffa2905bba1d228ffb2c205aae354621318a29
Vladimir Ermakov <vooon341@gmail.com>
2020-10-11 20:19:22 +0000

    Rebase from 'upstream'

--- pymavlink/examples/mavtest.py
+++ pymavlink/examples/mavtest.py
@@ -28,17 +28,17 @@ def test_protocol(mavlink, signing=False):
     mav = mavlink.MAVLink(f)
 
     if signing:
-        mav.signing.secret_key = chr(42)*32
+        mav.signing.secret_key = bytearray(chr(42)*32, 'utf-8' )
         mav.signing.link_id = 0
         mav.signing.timestamp = 0
         mav.signing.sign_outgoing = True
 
     # set the WP_RADIUS parameter on the MAV at the end of the link
-    mav.param_set_send(7, 1, "WP_RADIUS", 101, mavlink.MAV_PARAM_TYPE_REAL32)
+    mav.param_set_send(7, 1, b"WP_RADIUS", 101, mavlink.MAV_PARAM_TYPE_REAL32)
 
     # alternatively, produce a MAVLink_param_set object 
     # this can be sent via your own transport if you like
-    m = mav.param_set_encode(7, 1, "WP_RADIUS", 101, mavlink.MAV_PARAM_TYPE_REAL32)
+    m = mav.param_set_encode(7, 1, b"WP_RADIUS", 101, mavlink.MAV_PARAM_TYPE_REAL32)
 
     m.pack(mav)
 
